9cf4021ee3a4612a8be48bafe78b41bb88b11bef
Irfan <assidiq.irfan@gmail.com>
2018-10-20 01:20:27 +0700

    add project tugashusna1

---
+++ src/TugasHusna1/MainClass.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TugasHusna1;
+
+/**
+ *
+ * @author irfanasidiq
+ */
+public class MainClass {
+        /**
+         * hai husna, ini PR kamu yaaa
+         * :D
+         * intruksinya ada disela" kodingannya ya :D
+         * ada beberapa kelas yg aku buat, kamu coba pahami dan coba cari cara memperbaikinya ya,
+         * boleh searching tapi jangan copas yaa, harus ngetik !!!
+         */
+    public static void main(String args[]){
+        // Buatlah Object kucing !
+        // kemudian input parameter sesuai parameter yang dibutuhkan.
+        // tampilan yang diharapkan dari output kucing adalah:
+        // "nama kucing: anggora, usia 5 tahun."
+        // tulis kodingannya dibawah ini ya naa
+        
+        
+        
+        
+        
+        //coba identifikasi kodingan berikut na
+        Semut semut = new Semut();
+        semut.setNama("rangrang");
+        
+        System.out.println("jenis semut : " +semut.getNama());
+        // jelasin hasil analisanya dibawah ini ya na
+        /**
+         * disini jelasinnya ya naa nanti jelasin terus tinggal enter" aja kalo mau garis baru
+         * 
+         * 
+         */
+    }
+}
