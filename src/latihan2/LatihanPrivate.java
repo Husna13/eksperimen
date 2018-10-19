a14727f5a35d2165f2eadfbbf260a1b9728aa97f
Husna13 <muthmainnahhusna@gmail.com>
2018-10-19 20:48:38 +0700

    latihan private

---
+++ src/latihan2/LatihanPrivate.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan2;
+
+/**
+ *
+ * @author Husna Muthmainnah
+ */
+public class LatihanPrivate {
+    private String mahasiswa;
+    private String dosen;
+    private int nilai;
+    
+    public void setMahasiswa(String nama){ // nama itu parameter // set buat input nilai
+        mahasiswa = nama;
+    }
+    public String getMahasiswa(){
+        return mahasiswa;
+    }
+    
+}
